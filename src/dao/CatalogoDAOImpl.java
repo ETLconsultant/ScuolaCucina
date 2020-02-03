b053e2f53705e87def2346d84f511aea2c7839bc
First <First@192.168.21.230>
2020-02-03 10:03:38 +0100

    completato catalogoDao.

--- src/dao/CatalogoDAOImpl.java
+++ src/dao/CatalogoDAOImpl.java
@@ -1,12 +1,16 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entity.Categoria;
 import entity.Corso;
 import entity.Feedback;
+import entity.Utente;
 import exceptions.ConnessioneException;
 
 public class CatalogoDAOImpl implements CatalogoDAO {
@@ -22,8 +26,15 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void insert(Corso corso) throws SQLException {
-		// TODO Auto-generated method stub
-
+		PreparedStatement ps=conn.prepareStatement("INSERT INTO catalogo(id_corso, titolo, id_categoria, numeromaxpartecipanti, costo, descrizione) VALUES (?,?,?,?,?,?)");
+		ps.setInt(1, corso.getCodice());
+		ps.setString(2, corso.getTitolo());
+		ps.setInt(3, corso.getIdCategoria() );
+		ps.setInt(4, corso.getMaxPartecipanti());
+		ps.setDouble(5, corso.getCosto());
+		ps.setString(6, corso.getDescrizione());
+		
+		ps.executeUpdate();
 	}
 
 	/*
@@ -33,7 +44,16 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void update(Corso corso) throws SQLException {
-		// TODO Auto-generated method stub
+		PreparedStatement ps=conn.prepareStatement("UPDATE catalogo SET titolo=?, id_categoria=?, numeromaxpartecipanti=?, costo=?, descrizione=? where id_corso=?");
+		ps.setInt(6, corso.getCodice());
+		ps.setString(1, corso.getTitolo());
+		ps.setInt(2, corso.getIdCategoria() );
+		ps.setInt(3, corso.getMaxPartecipanti());
+		ps.setDouble(4, corso.getCosto());
+		ps.setString(5, corso.getDescrizione());
+		int n = ps.executeUpdate();
+		if(n==0)
+			throw new SQLException("corso: " + corso.getCodice() + " non presente");
 
 	}
 
@@ -45,8 +65,12 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public void delete(int idCorso) throws SQLException {
-		// TODO Auto-generated method stub
-
+		
+		PreparedStatement ps = conn.prepareStatement("DELETE FROM catalogo WHERE id_corso=?");
+		ps.setInt(1, idCorso);
+		int n = ps.executeUpdate();
+		if(n==0)
+			throw new SQLException("corso con id " + idCorso + " non presente");
 	}
 
 	/*
@@ -55,8 +79,26 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public ArrayList<Corso> select() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Corso> corsi = new ArrayList<Corso>(); 
+
+		PreparedStatement ps=conn.prepareStatement("SELECT * FROM catalogo");
+
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()){
+			
+			String titolo= rs.getString("titolo");
+			int id_categoria= rs.getInt("id_categoria");
+			int numeromaxpartecipanti= rs.getInt("numeromaxpartecipanti");
+			double costo =  rs.getDouble("costo");
+			String descrizione= rs.getString("descrizione");
+			
+
+			Corso corso = new Corso( titolo, id_categoria, numeromaxpartecipanti, costo, descrizione);
+			corsi.add(corso);
+		}
+
+		return corsi;
 	}
 
 	/*
@@ -65,9 +107,29 @@ public class CatalogoDAOImpl implements CatalogoDAO {
 	 */
 	@Override
 	public Corso select(int idCorso) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement ps=conn.prepareStatement("SELECT * FROM catalogo where id_corso =?");
+
+		ps.setInt(1, idCorso);
+
+		ResultSet rs = ps.executeQuery();
+		Corso corso =null;
+		
+		if(rs.next()){
+			String titolo= rs.getString("titolo");
+			int id_categoria= rs.getInt("id_categoria");
+			int numeromaxpartecipanti= rs.getInt("numeromaxpartecipanti");
+			double costo =  rs.getDouble("costo");
+			String descrizione= rs.getString("descrizione");
+
+			corso = new Corso(titolo, id_categoria, numeromaxpartecipanti, costo, descrizione);
+			return corso;
+		}
+		else
+			throw new SQLException("corso: " + idCorso + " non presente");
 	}
+	
+	
 
 
 }
