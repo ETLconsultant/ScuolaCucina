8bc577b33c81accd871afb63de0a3128f78f74f7
first <first@192.168.21.142>
2020-02-04 15:51:07 +0100

    test dao

---
+++ src/dao/DaoTest.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.sql.SQLException;
+
+import exceptions.ConnessioneException;
+
+public class DaoTest {
+
+	public static void main(String[] args) throws ConnessioneException, SQLException {
+		// TODO Auto-generated method stub
+		CalendarioDAOImpl cal = new CalendarioDAOImpl();
+		
+		
+		cal.delete(92);
+	}
+
+}
